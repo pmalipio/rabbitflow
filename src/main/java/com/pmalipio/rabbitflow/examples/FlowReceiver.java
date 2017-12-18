0c95cb393e82aef247520efed4b4beb517cd2cae
Pedro Alipio <pedro.alipio@gmail.com>
2017-12-18 22:56:11 +0000

    Added copyright notice in source files

--- src/main/java/com/pmalipio/rabbitflow/examples/FlowReceiver.java
+++ src/main/java/com/pmalipio/rabbitflow/examples/FlowReceiver.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2017.  Pedro Alípio, All Rights Reserved.
+ *
+ * This material is provided "as is", with absolutely no warranty expressed
+ * or implied. Any use is at your own risk.
+ *
+ * Permission to use or copy this software for any purpose is hereby granted
+ * without fee. Permission to modify the code and to distribute modified
+ * code is also granted without any restrictions.
+ */
 package com.pmalipio.rabbitflow.examples;
 
 import com.pmalipio.rabbitflow.ReceiverProducer;
